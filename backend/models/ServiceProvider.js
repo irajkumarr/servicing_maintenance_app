a8fdec5d8c48e7dd6437678e2438e405a5e279df
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-07-09 16:56:09 +0545

    Add ServiceProvider model with schema definition and validation

--- backend/models/ServiceProvider.js
+++ backend/models/ServiceProvider.js
@@ -1,3 +1,49 @@
 const mongoose = require("mongoose");
 
-const serviceProviderSchema = new mongoose.Schema({}, { timestamps: true });
+const serviceProviderSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    profileImage: String,
+    experienceYears: Number,
+    servicesOffered: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Service",
+      },
+    ],
+
+    availabilityStatus: {
+      type: String,
+      enum: ["online", "offline", "busy"],
+      default: "offline",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const ServiceProvider = mongoose.model(
+  "ServiceProvider",
+  serviceProviderSchema
+);
+module.exports = ServiceProvider;
