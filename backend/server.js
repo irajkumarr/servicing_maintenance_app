60ba1dddac6a10acd332ff99886521f0cf6e6f05
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-07-09 11:57:10 +0545

    Initialize server setup and add database connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+//PORT
+const PORT = process.env.PORT || 5000;
+
+//routes
+
+
+//database connection
+mongoose
+  .connect(process.env.MONGODB_CLOUD_URL)
+  .then(() => {
+    console.log("Connected to MongoDB! ✅");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
+//starting server on port
+app.listen(PORT, () => {
+  console.log(`Server running at PORT ${PORT} 🚀`);
+});
