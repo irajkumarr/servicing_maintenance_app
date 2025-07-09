ccef8b195c2f22a01902623cc40dc60d38f140e9
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-07-09 13:46:46 +0545

    Add User model with validation and password hashing; update server file structure

---
+++ backend/models/User.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minLength: [3, "Name must be at least 3 characters long."],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address.",
+      ],
+    },
+    phoneNumber: {
+      type: String,
+      unique: true,
+      required: [true, "Phone number is required."],
+      match: [/^\d{10}$/, "Phone number must be exactly 10 digits."],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [6, "Password must be 6 characters long."],
+      maxLength: [50, "Password cannot be more than 50 characters long."],
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    userType: {
+      type: String,
+      required: true,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    otp: {
+      type: String,
+    },
+    otpExpiry: {
+      type: Date,
+    },
+    verification: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified(password)) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  try {
+    const isMatch = await bcrypt.compare(enteredPassword, this, password);
+    return isMatch;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 //PORT
 const PORT = process.env.PORT || 5000;
 
 //routes
-
 
 //database connection
 mongoose
@@ -19,6 +19,7 @@ mongoose
     console.error("MongoDB connection error:", error);
     process.exit(1);
   });
+
 //starting server on port
 app.listen(PORT, () => {
   console.log(`Server running at PORT ${PORT} 🚀`);
