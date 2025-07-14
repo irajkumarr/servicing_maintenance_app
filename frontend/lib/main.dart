b311917212148222ac2419e3f71352ea2f61c28b
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-07-14 22:13:10 +0545

    Add Firebase configuration and initialize Firebase in the app

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,16 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:frontend/app.dart';
+import 'package:frontend/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
- // Lock the app in portrait mode
-  await SystemChrome.setPreferredOrientations(
-    [DeviceOrientation.portraitUp],
-  );
+  // Lock the app in portrait mode
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   //Initialize Firebase
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
